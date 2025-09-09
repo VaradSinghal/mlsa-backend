7d384658059491fbfcf5f8e49c5c00b13a831a7a
VaradSinghal <varadsinghal1526@gmail.com>
2025-09-09 18:09:54 +0530

    hosting

--- server.js
+++ server.js
@@ -3,34 +3,33 @@ const cors = require('cors');
 const dotenv = require('dotenv');
 const connectDB = require('./config/database');
 
-// Load env vars
+
 dotenv.config();
 
-// Connect to database
+
 connectDB();
 
 const app = express();
 
-// Middleware
+
 app.use(cors());
 app.use(express.json());
 
-// Routes
+
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/tasks', require('./routes/tasks'));
 
-// Health check endpoint
+
 app.get('/api/health', (req, res) => {
   res.json({ message: 'Server is running!' });
 });
 
-// Error handling middleware
+
 app.use((err, req, res, next) => {
   console.error(err.stack);
   res.status(500).json({ message: 'Something went wrong!' });
 });
 
-// 404 handler - proper Express middleware
 app.use((req, res) => {
   res.status(404).json({ 
     message: 'Route not found',
